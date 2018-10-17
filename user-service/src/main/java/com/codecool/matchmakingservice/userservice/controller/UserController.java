8810cb0bffc045dcaeba4ec116ef14018707e13f
Kovács Máté <kismate90@gmail.com>
2018-10-17 10:12:45 +0200

    Refactored test to check for Media Type constant instead of hard-coded string as content type of the response, and refactored User Controller "produces" parameter accordingly.

--- user-service/src/main/java/com/codecool/matchmakingservice/userservice/controller/UserController.java
+++ user-service/src/main/java/com/codecool/matchmakingservice/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.codecool.matchmakingservice.userservice.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserController {
 
-    @GetMapping(path = "/user/{id}", produces = "application/json")
+    @GetMapping(path = "/user/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getUserById(@PathVariable int id){
         return new ResponseEntity<>("", HttpStatus.OK);
     }
--- user-service/src/test/java/com/codecool/matchmakingservice/userservice/controller/UserControllerTest.java
+++ user-service/src/test/java/com/codecool/matchmakingservice/userservice/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,7 +25,7 @@ public class UserControllerTest {
 
     @Test
     public void requestForUserByIdResponseTypeIsJsonWithUTF8Charset() throws Exception {
-        mockMvc.perform(get("/user/1")).andExpect(content().contentType("application/json;charset=UTF-8"));
+        mockMvc.perform(get("/user/1")).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
     }
 
 }
