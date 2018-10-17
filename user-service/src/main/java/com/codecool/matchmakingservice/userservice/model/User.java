90a2e021e07037c452fcb1d27b3acaa2ac5cc86a
Kovács Máté <kismate90@gmail.com>
2018-10-17 14:51:23 +0200

    Added id, name, password, email and elo fields to User with respectable getters and setters and an empty constructor.

--- user-service/src/main/java/com/codecool/matchmakingservice/userservice/model/User.java
+++ user-service/src/main/java/com/codecool/matchmakingservice/userservice/model/User.java
@@ -1,7 +1,64 @@
 package com.codecool.matchmakingservice.userservice.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private int elo;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getElo() {
+        return elo;
+    }
+
+    public void setElo(int elo) {
+        this.elo = elo;
+    }
 }
