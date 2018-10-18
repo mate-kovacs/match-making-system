70f03dab0c70cc6cc854927a46c6d4b44e45a0a7
Kovács Máté <kismate90@gmail.com>
2018-10-18 10:13:45 +0200

    Created JPA Repository class for User.

---
+++ user-service/src/main/java/com/codecool/matchmakingservice/userservice/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.matchmakingservice.userservice.Repository;
+
+import com.codecool.matchmakingservice.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}
