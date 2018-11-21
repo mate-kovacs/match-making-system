3899c2ac0f49512b72433fbe95f35cf58621748f
Kovács Máté <kismate90@gmail.com>
2018-11-21 18:57:14 +0100

    Added annotations, mock mvc and mock repository to test class.

--- user-service/src/test/java/com/codecool/matchmakingservice/userservice/controller/UserUpdateControllerTest.java
+++ user-service/src/test/java/com/codecool/matchmakingservice/userservice/controller/UserUpdateControllerTest.java
@@ -1,7 +1,29 @@
 package com.codecool.matchmakingservice.userservice.controller;
 
+import com.codecool.matchmakingservice.userservice.repository.UserRepository;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserUpdateController.class)
 public class UserUpdateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserUpdateController controller;
+
+    
 
 }
