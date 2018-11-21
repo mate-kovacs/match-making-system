ff5e1fa60e6564cb956deae690d8c30155ebc7e3
Kovács Máté <kismate90@gmail.com>
2018-11-21 19:32:47 +0100

    Created test to check if post request for new user with valid data, then response is "ok", and changed controller endpoint to expect request body as parameter and always respond with "ok" to pass.

--- user-service/src/main/java/com/codecool/matchmakingservice/userservice/controller/UserUpdateController.java
+++ user-service/src/main/java/com/codecool/matchmakingservice/userservice/controller/UserUpdateController.java
@@ -1,11 +1,13 @@
 package com.codecool.matchmakingservice.userservice.controller;
 
+import com.codecool.matchmakingservice.userservice.model.User;
 import com.codecool.matchmakingservice.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserUpdateController {
@@ -14,8 +16,7 @@ public class UserUpdateController {
     UserRepository repository;
 
     @PostMapping(path = "/user")
-    public ResponseEntity<String> addNewUser() {
-        String message = "Data is missing.";
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> addNewUser(@RequestBody String user) {
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
--- user-service/src/test/java/com/codecool/matchmakingservice/userservice/controller/UserUpdateControllerTest.java
+++ user-service/src/test/java/com/codecool/matchmakingservice/userservice/controller/UserUpdateControllerTest.java
@@ -1,6 +1,9 @@
 package com.codecool.matchmakingservice.userservice.controller;
 
+import com.codecool.matchmakingservice.userservice.model.User;
+import com.codecool.matchmakingservice.userservice.model.UserStatus;
 import com.codecool.matchmakingservice.userservice.repository.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -27,9 +30,43 @@ public class UserUpdateControllerTest {
     @InjectMocks
     private UserUpdateController controller;
 
+    private User adam;
+    private User cindy;
+    private User wendy;
+
+    @Before
+    public void setup() {
+        adam = new User();
+        adam.setId(1L);
+        adam.setName("Adam");
+        adam.setEmail("adam@mms.com");
+        adam.setPassword("password1");
+        adam.setStatus(UserStatus.OFFLINE);
+        adam.setElo(100);
+        wendy = new User();
+        wendy.setId(2L);
+        wendy.setName("Wendy");
+        wendy.setEmail("wendy@mms.com");
+        wendy.setPassword("password2");
+        wendy.setStatus(UserStatus.OFFLINE);
+        wendy.setElo(125);
+        cindy = new User();
+        cindy.setId(3L);
+        cindy.setName("Cindy");
+        cindy.setEmail("cindy@mms.com");
+        cindy.setPassword("password3");
+        cindy.setStatus(UserStatus.ONLINE);
+        cindy.setElo(150);
+    }
+
     @Test
     public void postRequestNewUserIntoDatabaseWithMissingBodyRespondsBadRequestAndMissingDataMessage() throws Exception {
-        mockMvc.perform(post("/user")).andExpect(status().isBadRequest()).andExpect(content().string("Data is missing."));
+        mockMvc.perform(post("/user")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void postRequestNewUserIntoDatabaseWithValidInputRespondsOk() throws Exception {
+        mockMvc.perform(post("/user").content(adam.toJSonString())).andExpect(status().isOk());
     }
 
 }
