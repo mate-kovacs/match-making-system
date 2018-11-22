62b798b417e3c5f2f13e54d51583e1bed8a4fab4
Kovács Máté <kismate90@gmail.com>
2018-11-22 16:28:51 +0100

    Created test to check if get user from json is called with null parameter, then throws invalid user parameter exception with "Invalid user data." as message, and created a method in user service that always throws such an exception to pass.

--- user-service/src/main/java/com/codecool/matchmakingservice/userservice/service/UserService.java
+++ user-service/src/main/java/com/codecool/matchmakingservice/userservice/service/UserService.java
@@ -1,7 +1,13 @@
 package com.codecool.matchmakingservice.userservice.service;
 
+import com.codecool.matchmakingservice.userservice.model.InvalidUserParameterException;
+import com.codecool.matchmakingservice.userservice.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    public User getUserFromJson(String userJson) throws RuntimeException {
+        throw new InvalidUserParameterException("Invalid user data.");
+    }
 }
--- user-service/src/test/java/com/codecool/matchmakingservice/userservice/service/UserServiceTest.java
+++ user-service/src/test/java/com/codecool/matchmakingservice/userservice/service/UserServiceTest.java
@@ -1,5 +1,10 @@
 package com.codecool.matchmakingservice.userservice.service;
 
+import com.codecool.matchmakingservice.userservice.model.InvalidUserParameterException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -7,5 +12,21 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 public class UserServiceTest {
-    
+
+    private UserService service;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        service = new UserService();
+    }
+
+    @Test
+    public void getUserFromJsonForNullParameterThrowsExceptionWithMessageInvalidInput() {
+        exceptionRule.expect(InvalidUserParameterException.class);
+        exceptionRule.expectMessage("Invalid user data.");
+        service.getUserFromJson(null);
+    }
 }
