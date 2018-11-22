b42aa27900e765152861640cf100ab133e6fbded
Kovács Máté <kismate90@gmail.com>
2018-11-22 16:13:27 +0100

    Created user service class and the respective test class.

---
+++ user-service/src/test/java/com/codecool/matchmakingservice/userservice/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.codecool.matchmakingservice.userservice.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class UserServiceTest {
+    
+}
