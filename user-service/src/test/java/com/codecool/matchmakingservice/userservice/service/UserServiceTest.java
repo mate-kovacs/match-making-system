ae7d7f3d6a20f13c5ef96c3e9701a91b015cd9f0
Kovács Máté <kismate90@gmail.com>
2018-11-22 16:31:32 +0100

    Created test to check if get user from json is called with not a valid json string then throwss invalid user parameter exception with message "Invalid user data.", and no production code was needed to pass.

--- user-service/src/test/java/com/codecool/matchmakingservice/userservice/service/UserServiceTest.java
+++ user-service/src/test/java/com/codecool/matchmakingservice/userservice/service/UserServiceTest.java
@@ -29,4 +29,11 @@ public class UserServiceTest {
         exceptionRule.expectMessage("Invalid user data.");
         service.getUserFromJson(null);
     }
+
+    @Test
+    public void getUserFromJsonForNotJsonStringParameterThrowsExceptionWithMessageInvalidInput() {
+        exceptionRule.expect(InvalidUserParameterException.class);
+        exceptionRule.expectMessage("Invalid user data.");
+        service.getUserFromJson("{name:'Adam', pass");
+    }
 }
