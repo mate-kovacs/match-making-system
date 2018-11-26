415dde0c599777a0703e80c56ab46efa6ea26b54
Kovács Máté <kismate90@gmail.com>
2018-11-26 13:54:33 +0100

    Created a test to check if /login route is called with a get request, response has media type 'text/html' and 'charset utf-8', and no production code was needed to pass.

--- ui-service/test/controller_test.py
+++ ui-service/test/controller_test.py
@@ -15,3 +15,7 @@ class ControllerTest(unittest.TestCase):
         response = self.app.get('/login', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_login_route_route_response_html_with_utf8_charset(self):
+        response = self.app.get('/login', follow_redirects=True)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+
