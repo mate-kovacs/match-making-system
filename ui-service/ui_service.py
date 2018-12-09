4568d3dacbff6a097c7df365255580bc0c5eeb66
Kovács Máté <kismate90@gmail.com>
2018-12-09 18:42:28 +0100

    Created login form and added it to the login template to use instead of the dummy html form.

--- ui-service/ui_service.py
+++ ui-service/ui_service.py
@@ -5,8 +5,13 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "I dont know what to put in here."
 Bootstrap(app)
 
+class LoginForm(FlaskForm):
+    email = StringField("email", validators=[InputRequired, Length(max=50)])
+    password = PasswordField("password", validators=[InputRequired, Length(min=6 , max=80)])
+    remember = BooleanField("remember me")
 
 @app.route('/')
 def hello_world():
@@ -15,7 +20,9 @@ def hello_world():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    return render_template('login.html', form = form)
 
 
 @app.route('/register')
