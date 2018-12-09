50f7947ffe3a9a9f55651871e8d7db028ab914ff
Kovács Máté <kismate90@gmail.com>
2018-12-09 18:21:27 +0100

    Created basic routes rendering templates and imported various flask accessories for the flask app.

--- ui-service/ui_service.py
+++ ui-service/ui_service.py
@@ -1,21 +1,37 @@
-from flask import Flask, redirect
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
-    return redirect('/login', code=302)
+    return redirect(url_for('login'))
 
 
 @app.route('/login')
-def login_route():
-    return ''
+def login():
+    return render_template('login.html')
+
 
 @app.route('/register')
-def register_route():
+def register():
+    return render_template('register.html')
+
+
+@app.route('/lobby')
+def lobby():
+    return render_template('lobby.html')
+
+
+@app.route('/logout')
+def logout():
     return ''
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
